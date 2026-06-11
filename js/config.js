b847a02e610e99e03a1844b8aba689c2230f25ba
PpAoO <bunyaphon2911@gmail.com>
2026-06-11 16:01:52 +0700

    Update script URLs in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
  // Configuration for Google Apps Script API
 // ใช้ลิงก์เดียวกันทั้งหมดสำหรับทุกหน้า เนื่องจากมีฐานข้อมูล (Script) ชุดเดียว
-const scriptURL = "https://script.google.com/macros/s/AKfycbxTUXQyfcacNjPY4AcbU22_ccmhFDEcGpQtVOu4L6CXqa5CMy9XECweaGdvAnniUSoO/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbytog1dBHRLwY5_tpBs8daT9HY4F2YJ7DihGMR61w8RiZwtU0laaXUlIzaHBRo35P5u4Q/exec";
 const scriptURLBKK = "https://script.google.com/macros/s/AKfycbwu-sOPhox_aoQh5nKfRapmjiZFDZ5ShAhw51dXwiBjzf302rDK5KKnabGM2n_jZFzZlA/exec";
 
 const authScriptURL = scriptURL;
